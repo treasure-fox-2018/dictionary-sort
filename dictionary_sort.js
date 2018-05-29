f4eab97696938cd8172fdd90f48ff63068d7fdf7
dawimuhammad <muhamadhaddawi@gmail.com>
2018-05-30 00:22:04 +0700

    done exercise dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+/**
+ * @function dictionarySort
+ * melaksanakan pengurutan string alphabetic
+ * @param {array} inputs kumpulan kata-kata acak
+ * @returns {string} mengembalikan kumpulan kata-kata yang sudah diurutkan dalam bentuk string
+ */
+
+function dictionarySort(inputs) {
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i] > inputs[i+1]) {
+            let str = inputs[i];
+            inputs[i] = inputs[i+1];
+            inputs[i+1] = str;
+        }
+    }
+
+    return inputs.join();
+}
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
+
+module.exports = { dictionarySort };
