88fc2b476613e93c8bce172f6f053f1f68224442
fajartc02 <fajartc02@gmail.com>
2018-05-29 22:05:50 +0700

    submit dictionary-sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function dictionarySort (arrOfWords) {
+  for (var a = 0; a < arrOfWords.length; a ++) {
+    for(var b = a+1; b < arrOfWords.length; b++) {
+      if(arrOfWords[b] < arrOfWords[a]) {
+        var tampung = arrOfWords[a];
+        arrOfWords[a] = arrOfWords[b];
+        arrOfWords[b] = tampung
+      }
+    }
+  }
+  var joinArr = arrOfWords.join(',')
+  return joinArr
+}
 
-module.exports = Dictionary
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']))
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']))
+
+// module.exports = Dictionary
