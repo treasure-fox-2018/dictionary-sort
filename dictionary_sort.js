205a0c40deb3870da7df8cb95e9f01eb238478b9
Your Name <fahrisyany@gmail.com>
2018-05-29 20:19:01 +0700

    challange finished

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,30 @@
 const readline = require('readline');
+
+function dicSort(arr) {
+
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = i; j >= 0; j--) {
+            if (arr[i] < arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+
+    }
+    var str = ''
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i] + ', '
+
+    }
+
+    return str.substring(0,str.length-2)
+}
+
+console.log(dicSort(['makan', 'duduk', 'tidur', 'terbang']))
+
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+// module.exports = Dictionary
