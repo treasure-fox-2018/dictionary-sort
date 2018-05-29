5649cebc412e1a0370f84b110999449a946d0bf4
handipriyono <handimacbook@gmail.com>
2018-05-29 20:40:43 +0700

    add dict sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,23 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
+function Dictionary(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i].toLowerCase() > arr[j].toLowerCase()) {
+        var kc = arr[j];
+        arr[j] = arr[i];
+        arr[i] = kc;
+      }
+    }
+  }
+
+  return arr.join(',')
+}
+
+var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+var arrOfWord1 = ['anggi', 'angga', 'ani', 'adi'];
+console.log(Dictionary(arrOfWord)); // duduk,makan,terbang,tidur
+console.log(Dictionary(arrOfWord1)); // adi,angga,anggi,ani
+
 module.exports = Dictionary
