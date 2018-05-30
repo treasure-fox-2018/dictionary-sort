5a3963fb5ddb8937ed85454b2b74a73a002d7185
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-05-30 21:52:18 +0700

    finish dictionary_sort.js

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,23 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    var tempoSmallest = arr[i];
+    var tempoSmallestPosition = 0;
+    for (var j = i; j < arr.length; j++) {
+      var comparison = arr[j]
+      if (comparison < tempoSmallest) {
+        tempoSmallest = arr[j];
+        tempoSmallestPosition = j;
+      }
+    }
+    arr[tempoSmallestPosition] = arr[i];
+    arr[i] = tempoSmallest;
+  }
+  return arr.join(", ");
+}
+
+console.log(selectionSort(["anggi", "angga", "ani", "adi"]));
+
+// module.exports = Dictionary
